4bcf0cf07546b88a80b80003eb2beee2a2da28b7
Charana Nandasena <an15739@bc4login3.bc4.acrc.priv>
2019-04-08 14:24:44 +0100

    new

--- dataset/SNAP/directed_to_sc.cpp
+++ dataset/SNAP/directed_to_sc.cpp
@@ -25,73 +25,84 @@ void time(string name, std::function<void()> func){
     cout << endl;
 }
 
+const int ts = 28;
 int main(int argc, char** argv){
-    time("graph to set-cover", [&]() -> void{
-        const int ts = 28;
-        string outpath = string(argv[1]);
-        string inpath = string(argv[2]);
-        int directed = stoi(argv[3]);
-        bip::file_mapping mapping(inpath.c_str(), bip::read_only);
-        unsigned long file_size = filesize(inpath.c_str());
-        unsigned long thread_block = file_size / ts;
-        unsigned long block_remain = file_size % ts;
 
-        unsigned long tlargest[ts] = {0};
-        unsigned long largest = 0;
-        vector<Edge>* tedges = new vector<Edge>[ts]();
-        vector<unsigned long>** sets = new vector<unsigned long>*[ts]();
-        #pragma omp parallel for
-        for(int t = 0; t < ts; t++){
-            bip::mapped_region mapped_rgn(mapping, bip::read_only, t * thread_block, thread_block + (t == ts-1 ? block_remain : 0));
-            char const* const mmaped_data = static_cast<char*>(mapped_rgn.get_address());
-            size_t const mmap_size = mapped_rgn.get_size();
+    // arguments
+    const string inpath = string("/mnt/storage/scratch/an15739/") + string(argv[1]);
+    const string outpath = string("/mnt/storage/scratch/an15739/") + string(argv[2]);
+    const int directed = stoi(argv[3]);
 
-            imemstream mmifs(mmaped_data, mmap_size);
-            for(string line; getline(mmifs, line); ){
-                boost::trim(line);
-                char *cline = new char[line.length() + 1];
-                /* cout << "[-" << line << "]" << endl; */
-                if(line.size() == 0) continue;
-                strcpy(cline, line.c_str());
-                const char* cs = std::strtok(cline, " \t");
-                /* cout << string(cs) << endl; */
-                unsigned long s = stoul(cs);
-                const char* cd = std::strtok(NULL, " \t");
-                if(cd == NULL) continue;
-                /* cout << string(cd) << endl; */
-                /* cout << "===" << endl; */
-                unsigned long d = stoul(cd);
-                delete[] cline;
-                if(directed == 1) { if(s > tlargest[t]) tlargest[t] = s; }
-                else { if(max(s, d) > tlargest[t]) tlargest[t] = max(s, d); }
-                tedges[t].push_back(Edge{s, d});
-            }
+    // memory mapped file
+    bip::file_mapping mapping(inpath.c_str(), bip::read_only);
+    bip::mapped_region mapped_rgn(mapping, bip::read_only);
+    char const* const mmaped_data = static_cast<char*>(mapped_rgn.get_address());
+    size_t const mmap_size = mapped_rgn.get_size();
+    imemstream mmifs(mmaped_data, mmap_size);
+
+    unsigned long largest = 0;
+    vector<Edge>* edges = new vector<Edge>();
+    time("sequential - reading file and parsing to edges", [&]() -> void{
+        for(string line; getline(mmifs, line); ){
+            boost::trim(line);
+            if(line.size() == 0) continue;
+            char *cline = new char[line.length() + 1];
+            strcpy(cline, line.c_str());
+            char* cs = std::strtok(cline, " \t");
+            unsigned long s = stoul(cs);
+            char* cd = std::strtok(NULL, " \t");
+            if(cd == NULL || strcmp(cd, "\0") == 0) continue;
+            unsigned long d = stoul(cd);
+            if(directed == 1) { if(s > largest) largest = s; }
+            else { if(max(s, d) > largest) largest = max(s, d); }
+            edges->push_back(Edge{s, d});
         }
-        for(int t = 0; t < ts; t++) if(tlargest[t] > largest) largest = tlargest[t];
-        /* cout << "here" << endl; */
+    });
+
+//    const unsigned long block = edges->size() / ts;
+//    Edge* edges_data = edges->data();
+//    vector<unsigned long>** sets = new vector<unsigned long>*[ts]();
+//    time("sequential - edges to sets", [&]() -> void{
+//        #pragma omp parallel for
+//        for(int t = 0; t < ts; t++){
+//            sets[t] = new vector<unsigned long>[largest+1]();
+//            for(int i = block * t; i < block * (t+1); i++){
+//                sets[t][(edges_data+i)->s].push_back((edges_data+i)->d);
+//                if(!(directed == 1)) sets[t][(edges_data+i)->d].push_back((edges_data+i)->s);
+//            }
+//        }
+//        for(int i = block * ts; i < edges->size(); i++){
+//            sets[0][(edges_data+i)->s].push_back((edges_data+i)->d);
+//            if(!(directed == 1)) sets[0][(edges_data+i)->d].push_back((edges_data+i)->s);
+//        }
+//    });
+
+    Edge* edges_data = edges->data();
+    vector<unsigned long>* sets = new vector<unsigned long>[largest+1]();
+    time("sequential - edges to sets", [&]() -> void{
+        for(int i = 0; i < edges->size(); i++){
+            sets[(edges_data+i)->s].push_back((edges_data+i)->d);
+            if(!(directed == 1)) sets[(edges_data+i)->d].push_back((edges_data+i)->s);
+        }
+    });
+
+    const unsigned long block = largest / ts;
+    const unsigned long block_remain = largest % ts;
+    time("parallel - write sets to files", [&]() -> void{
         #pragma omp parallel for
         for(int t = 0; t < ts; t++){
-            sets[t] = new vector<unsigned long>[largest+1]();
-            /* cout << "here1" << endl; */
-            for(Edge& e : tedges[t]){
-                sets[t][e.s].push_back(e.d);
-                if(!(directed == 1)) sets[t][e.d].push_back(e.s);
-            }
-        }
-        #pragma omp parallel for
-        for(int t = 0; t < ts; t++){
-            const unsigned long block = largest / ts;
             std::ofstream outfile(outpath + "." + to_string(t));
-            for(unsigned long i = t * block; i < (t+1) * block; i++){
+            for(unsigned long i = t * block; i < (t+1) * block + (t == ts-1) ? block_remain : 0; i++){
                 /* outfile << "i[" << i << "]" << " "; */
-                for(int t = 0; t < ts; t++) {
-                    for(unsigned long v : sets[t][i]){
-                        outfile << v << " ";
-                    }
+                if(sets[i].size() > 0){
+                    for(unsigned long v : sets[i]) outfile << v << " ";
+                    outfile << endl;
                 }
-                outfile << endl;
             }
         }
+    });
+
+    time("sequential - dump files to file", [&]() -> void{
         remove(outpath.c_str());
         std::ofstream outfile(outpath);
         for(int t = 0; t < ts; t++){
